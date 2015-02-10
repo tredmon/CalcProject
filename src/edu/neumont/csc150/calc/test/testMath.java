0703b298ae144fa46a09c0d74fc3ef41590a88ec
tredmon <trevan.redmon@gmail.com>
2015-02-10 10:50:54 -0700

    created test drivers

---
+++ src/edu/neumont/csc150/calc/test/testMath.java
@@ -0,0 +1,13 @@
+package edu.neumont.csc150.calc.test;
+
+public class testMath {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
