0a18b81dacb1ca74617c134781ea34048df11c58
Lockandkey <hopeful.genius@gmail.com>
2015-02-11 10:30:13 -0700

    Driver

---
+++ src/main/CalcProjectDriver.java
@@ -0,0 +1,21 @@
+package main;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import view.MyPanel;
+import model.Converters;
+
+public class Driver {
+	public static void main (String[] args) throws Exception {
+		JFrame frame = new JFrame("Converting between number systems");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel mainpanel = new MyPanel();
+		frame.getContentPane().add(mainpanel);
+		
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+}
