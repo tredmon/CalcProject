63fec0d3a99f853204a4259b75dea8815443d5bc
DawitK <dkebret@student.neumont.edu>
2015-02-15 11:18:04 -0700

    Finished all the conversion methods

--- src/edu/neumont/csc150/calc/test/TheMath.java
+++ src/edu/neumont/csc150/calc/test/TheMath.java
@@ -1,4 +1,5 @@
-import java.util.ArrayList;
+package edu.neumont.csc150.calc.test;
+
 
 
 public class TheMath {
@@ -36,22 +37,23 @@ public class TheMath {
 	
 //	private static ArrayList <Integer> list = new ArrayList <Integer>();
 	
-	public static String deciToBinary(int input){
+	public static String deciToBinary(String input){
 		String x = "";
+		int newinput = Integer.parseInt(input);
 		
-		while(input >= 1){
+		while(newinput >= 1){
 			
-			if(input % 2 == 0){
-				input = input/2;
+			if(newinput % 2 == 0){
+				newinput = newinput/2;
 				x += "0";
 			}
-			else if(input % 2 == 1){
-				input = input/2;
+			else if(newinput % 2 == 1){
+				newinput = newinput/2;
 				x += "1";
 			}
 		}
 		String z = "";
-		for(int i = x.length(); i > 0; i--){
+		for(int i = x.length() - 1; i >= 0; i--){
 				 z += x.charAt(i);
 		}
 		return z;
@@ -63,20 +65,28 @@ public class TheMath {
 			switch(input.charAt(x)){
 				case '0':
 					value += (o0);
+					break;
 				case '1':
 					value += (o1);
+					break;
 				case '2':
 					value += (o2);
+					break;
 				case '3':
 					value += (o3);
+					break;
 				case '4':
 					value += (o4);
+					break;
 				case '5':
 					value += (o5);
+					break;
 				case '6':
 					value += (o6);
+					break;
 				case '7':
 					value += (o7);
+					break;
 			}
 		}
 		return value;
@@ -88,38 +98,123 @@ public class TheMath {
 			switch(input.charAt(x)){
 			case '0':
 				value += (h0);
+				break;
 			case '1':
 				value += (h1);
+				break;
 			case '2':
 				value += (h2);
+				break;
 			case '3':
 				value += (h3);
+				break;
 			case '4':
 				value += (h4);
+				break;
 			case '5':
 				value += (h5);
+				break;
 			case '6':
 				value += (h6);
+				break;
 			case '7':
 				value += (h7);
+				break;
 			case '8':
 				value += (h8);
+				break;
 			case '9':
 				value += (h9);
+				break;
 			case 'a':
 				value += (ha);
+				break;
 			case 'b':
 				value += (hb);
+				break;
 			case 'c':
 				value += (hc);
+				break;
 			case 'd':
 				value += (hd);
+				break;
 			case 'e':
 				value += (he);
+				break;
 			case 'f':
 				value += (hf);
+				break;
 			}
 		}
 		return value;
+	}
+	
+	public static int binToDecimal(String input){
+		return Integer.parseInt(input, 2);
+	}
+	
+	public static int octToDecimal(String input){
+		return Integer.parseInt(input, 8);
+	}
+	
+	public static int hexToDecimal(String input){
+		return Integer.parseInt(input, 16);
+	}
+	
+	public static String deciToHex(String input){
+		String x = "";
+		int newinput = Integer.parseInt(input);
+		
+		while(newinput >= 1){
+			newinput = newinput/16;
+			x += newinput % 16;
+		}
+		String z = "";
+		for(int i = x.length() - 1; i >= 0; i--){
+				 z += x.charAt(i);
+		}
+		return z;
+	}
+	
+	public static int octToHex(String input){
+		return binToHex(octToBinary(input));
+	}
+	
+	public static int binToHex(String input){
+		int num = Integer.parseInt(input, 2);
+		String result = Integer.toHexString(num);
+		return Integer.parseInt(result, 16);
+	}
+	
+	public static String deciToOctal(String input){
+		String x = "";
+		int newinput = Integer.parseInt(input);
+		
+		while(newinput >= 1){
+			
+			if(newinput % 2 == 0){
+				newinput = newinput/8;
+				x += "0";
+			}
+			else if(newinput % 2 == 1){
+				newinput = newinput/8;
+				x += "1";
+			}
+		}
+		String z = "";
+		for(int i = x.length() - 1; i >= 0; i--){
+				 z += x.charAt(i);
+		}
+		return z;
+	}
+	
+	public static int hexToOctal(String input){
+		return binToOctal(hexToBinary(input));
+	}
+	
+	public static int binToOctal(String input){
+		int num = Integer.parseInt(input, 2);
+		String result = Integer.toOctalString(num);
+		return Integer.parseInt(result, 8);
 	}
 }
