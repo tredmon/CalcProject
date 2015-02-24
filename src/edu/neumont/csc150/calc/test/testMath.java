2756915c475f7866e5afc11ca1e9a82777455845
DawitK <dkebret@student.neumont.edu>
2015-02-24 11:03:06 -0700

    :dango:

--- src/edu/neumont/csc150/calc/test/testMath.java
+++ src/edu/neumont/csc150/calc/test/testMath.java
@@ -1,12 +1,18 @@
 package edu.neumont.csc150.calc.test;
 
+import java.io.IOException;
+
+import edu.neumont.csc150.payroll.view.IUserInterface;
+import edu.neumont.csc150.payroll.view.UserInterfaceFactory;
+
 public class testMath {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
-		IUserInterface ui = UserInterfaceFactories.getInstance();
+	public static void main(String[] args) throws IOException {
+		IUserInterface ui = UserInterfaceFactory.getInstance();
 		String input = ui.promptStringInput();
 		System.out.println(TheMath.deciToBinary(input));
 
