42ebdcbb8b2a4b5ddce527d02d14a75f5a15d0a4
DawitK <dkebret@student.neumont.edu>
2015-03-06 10:47:48 -0700

    Recreated TheMath
    Will redo the lost methods

--- src/edu/neumont/csc150/calc/test/testMath.java
+++ src/edu/neumont/csc150/calc/test/testMath.java
@@ -1,32 +1,21 @@
 package edu.neumont.csc150.calc.test;
 
-<<<<<<< HEAD
 import java.io.IOException;
 
 import model.TheMath;
 import edu.neumont.csc150.payroll.view.IUserInterface;
 import edu.neumont.csc150.payroll.view.UserInterfaceFactory;
 
-=======
->>>>>>> origin/master
 public class testMath {
 
 	/**
 	 * @param args
 	 */
-<<<<<<< HEAD
 	public static void main(String[] args) throws IOException {
 		IUserInterface ui = UserInterfaceFactory.getInstance();
 		System.out.println(":D");
 //		System.out.println(TheMath.hexToBinary(ui.promptStringInput()));
 		System.out.println(Integer.parseInt(TheMath.deciToBinary(Integer.toString(6)), 2) ^ Integer.parseInt(TheMath.deciToBinary(Integer.toString(5)), 2));
-=======
-//	public static void main(String[] args) {
-//		//IUserInterface ui = UserInterfaceFactories.getInstance();
-//		//String input = ui.promptStringInput();
-//		//System.out.println(TheMath.deciToBinary(input));
-//
-//>>>>>>> origin/master
-//	}
 
+	}
 }
