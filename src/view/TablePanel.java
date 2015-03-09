4f71e2bd965ea2fb4864deacc74fc5c5a89c98bc
tredmon <trevan.redmon@gmail.com>
2015-03-09 15:20:12 -0600

    quick update to tablepanel

--- src/view/TablePanel.java
+++ src/view/TablePanel.java
@@ -15,135 +15,98 @@ public class TablePanel extends JPanel{
 
 	JTable BoundsTable;
 	JTable XYTable; 
+	JScrollPane xyscroller;
+	GraphPanel par;
 	/**
 	 * @param args
 	 */
-	public TablePanel()
-	
-	
-	
+	public TablePanel(GraphPanel parent)
 	{
 		super();
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		this.setPreferredSize(new Dimension(550,260));
 		this.setSize(new Dimension(550,260));
 		MinVal(0.0, 10.0, 2.0, 10.0, 5.0);
-		
+
 		JLabel label = new JLabel("Table View");
 		label.setVerticalAlignment(JLabel.CENTER);		
 		add(label);
-		
-		
-		
-		
+
+		setTable(new Double[][]{new Double[]{0.0, 0.0}});
+	}
+	public void setTable(Double[][] tablevals){
 		String[] columnNames = new String[2];
 		columnNames[1] = "Y";
 		columnNames[0] = "X";
+
+		XYTable = new JTable(tablevals,columnNames);
 		
-		
-		int NumofItems = 5;
-		Double[][] x = new Double[NumofItems][2]; 
-		for(int R = 0; R < NumofItems; R++)
-		{
-			
-			for(int C = 0; C < columnNames.length; C++)
-			{
-				x[R][C] = 2.0;
-				
-				
-			}
+		if(xyscroller != null){
+			remove(xyscroller);
 		}
-		XYTable = new JTable(x,columnNames);
-		
-		JScrollPane scrollPane = new JScrollPane(XYTable);
+		xyscroller = new JScrollPane(XYTable);
 		XYTable.setFillsViewportHeight(true);
-		
-		
-		
-	
-		
-		add(scrollPane);
+		add(xyscroller);
 		XYTable.getColumnModel().getColumn(0).setCellEditor(new noEditor());
 		XYTable.getColumnModel().getColumn(1).setCellEditor(new noEditor());
-	}
-		public void  MinVal(Double MinX, Double MaxX, Double MinY, Double MaxY, Double DeltaX)
-		
-		{
-			
-			
-			
-			JLabel label = new JLabel("Bounds");
-			label.setHorizontalAlignment(JLabel.CENTER);
-			add(label);
-			
-			String[] columnNames = new String[2];
-			columnNames[0] = "";
-			columnNames[1] = "";
-			
-			Object[][] row = new Object[5][2];
-			row[0][0] = "MinX";
-			row[1][0] = "MaxX";
-			row[2][0] = "MinY";
-			row[3][0] = "MaxY";
-			row[4][0] = "DeltaX";
-			
-			row[0][1] = MinX.toString();
-			row[1][1] = MaxX.toString();
-			row[2][1] = MinY.toString();
-			row[3][1] = MaxY.toString();
-			row[4][1] = DeltaX.toString();
-			
-			BoundsTable = new JTable(row, columnNames);
-			add(BoundsTable);
-			
-			
-			BoundsTable.getColumnModel().getColumn(0).setCellEditor(new noEditor());
-			
-		}
-		public static class noEditor extends DefaultCellEditor
-		{
-			public noEditor()
-			{
-				super(new JTextField());
-				
-				
-			}
-			public Object getCellEditorValue()
-			{
-				return null;
-			}
-			@Override
-			public Component getTableCellEditorComponent(JTable table, Object object, boolean Boolean, int INT1, int INT2)
-			{
-				return null;
-				
-			}
-			
-			
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 
 	}
-		public JTable getMinTable()
+	public void  MinVal(Double MinX, Double MaxX, Double MinY, Double MaxY, Double DeltaX)
+	{
+		JLabel label = new JLabel("Bounds");
+		label.setHorizontalAlignment(JLabel.CENTER);
+		add(label);
+
+		String[] columnNames = new String[2];
+		columnNames[0] = "";
+		columnNames[1] = "";
+
+		Object[][] row = new Object[5][2];
+		row[0][0] = "MinX";
+		row[1][0] = "MaxX";
+		row[2][0] = "MinY";
+		row[3][0] = "MaxY";
+		row[4][0] = "DeltaX";
+
+		row[0][1] = MinX.toString();
+		row[1][1] = MaxX.toString();
+		row[2][1] = MinY.toString();
+		row[3][1] = MaxY.toString();
+		row[4][1] = DeltaX.toString();
+
+//		if()
+		BoundsTable = new JTable(row, columnNames);
+		add(BoundsTable);
+
+
+		BoundsTable.getColumnModel().getColumn(0).setCellEditor(new noEditor());
+		//TableModelListener applied to column 1 
+		//BoundsTable.get
+		//
+	}
+	public static class noEditor extends DefaultCellEditor
+	{
+		public noEditor()
 		{
-			return BoundsTable;
-			
-			
+			super(new JTextField());
 		}
-		public JTable getXYTable()
+		public Object getCellEditorValue()
 		{
-			return XYTable;
+			return null;
 		}
+		@Override
+		public Component getTableCellEditorComponent(JTable table, Object object, boolean Boolean, int INT1, int INT2)
+		{
+			return null;
+		}
+	}
+	public JTable getMinTable()
+	{
+		return BoundsTable;
+	}
+	public JTable getXYTable()
+	{
+		return XYTable;
+	}
 
 }
