befc0de5ab2ba66f70bba8a10041c8df14d4a1aa
DawitK <dkebret@student.neumont.edu>
2015-03-11 20:15:58 -0600

    Tweaks

--- src/Model/TheMath.java
+++ src/Model/TheMath.java
@@ -183,17 +183,18 @@ private TheMath(){}
 	}
 	
 	public static String deciToHex(String input){
-		return Integer.toHexString((int)Double.parseDouble(input))/*.toUpperCase()*/;
+		return Integer.toHexString((int)Double.parseDouble(input)).toUpperCase();
 	}
 	
 	public static String octToHex(String input){
-		return Integer.toOctalString((int)Double.parseDouble(input))/*.toUpperCase()*/;
+		String x = Integer.toOctalString((int)Double.parseDouble(input)).toUpperCase();
+		return Integer.toHexString((int)Double.parseDouble(x)).toUpperCase();
 	}
 	
 	public static String binToHex(String input){
 		int num = Integer.parseInt(input, 2);
 		String result = Double.toHexString(num);
-		return result/*.toUpperCase()*/;
+		return result.toUpperCase();
 	}
 	
 	public static String deciToOctal(String input){
